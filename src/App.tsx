cc2d399f9fa3b2be980923e059daa7a82c606cc9
José Brito <95869529+JoseBrittoo@users.noreply.github.com>
2025-10-06 18:08:54 -0400

    firt commit

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Bounce, ToastContainer } from "react-toastify";
+
+//pages imports
+import LoginPage from "./modules/auth/pages/login-page/LoginPage";
+import RegisterPage from "./modules/auth/pages/register-page/RegisterPage";
+//
+
+const routes = [
+  {
+    path: "/",
+    element: <LoginPage />, 
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  }
+];
+
+const router = createBrowserRouter(routes);
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+      <ToastContainer
+        theme="light"
+        toastClassName={
+          "relative flex p-0 min-h-10 rounded-md overflow-hidden cursor-pointer"
+        }
+        position="top-right"
+        autoClose={5000}
+        transition={Bounce}
+        pauseOnHover={false}
+        closeOnClick
+        hideProgressBar
+        newestOnTop
+      />
+    </>
+  );
+}
+
+export default App;
---
+++ src/modules/auth/pages/login-page/LoginPage.tsx
@@ -0,0 +1,3 @@
+export default function LoginPage() {
+  return <div className="text-fuchsia-700">Login Page</div>;
+}
