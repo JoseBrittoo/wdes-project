001dff3dd849fe78facca5d2222f84c6e4ae7eef
José Brito <95869529+JoseBrittoo@users.noreply.github.com>
2025-10-30 14:05:41 -0400

    Ajustes de importação

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import LoginPage from "./modules/auth/pages/login-page/LoginPage";
 import RegisterPage from "./modules/auth/pages/register-page/RegisterPage";
 import DashboardStudents from "./modules/dashboards/pages/DashboardStudants";
 import DashboardProfessor from "./modules/dashboards/pages/DashboardProfessor";
-import DisciplinaGrade from "./modules/disciplinaGrade/pages/DisciplinaGradePage";
+import DisciplinaGrade from "./modules/DisciplinaGrade/pages/DisciplinaGradePage";
 import Studants from "./modules/studants/pages/Studants";
 import GradeCurricularPage from "./modules/grade/pages/GradeCurricularPage";
 import Professor from "./modules/professor/pages/Professor";
